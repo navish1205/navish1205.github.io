a982d8d76a18a824eda05992bf56b2a107e3878c
navish davedi <navish1205@gmail.com>
2017-12-05 10:30:16 +0530

    added all files

---
+++ main.js
@@ -0,0 +1,39 @@
+function aboutfx () {
+  document.getElementById('about').style.display = 'block';
+  document.getElementById('education').style.display = 'none';
+  document.getElementById('personal-projects').style.display = 'none';
+  document.getElementById('freelancing-projects').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+}
+
+function educationfx () {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('education').style.display = 'block';
+  document.getElementById('personal-projects').style.display = 'none';
+  document.getElementById('freelancing-projects').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+}
+
+function perprofx () {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('education').style.display = 'none';
+  document.getElementById('personal-projects').style.display = 'block';
+  document.getElementById('freelancing-projects').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+}
+
+function freeprofx () {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('education').style.display = 'none';
+  document.getElementById('personal-projects').style.display = 'none';
+  document.getElementById('freelancing-projects').style.display = 'block';
+  document.getElementById('contact').style.display = 'none';
+}
+
+function contactfx () {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('education').style.display = 'none';
+  document.getElementById('personal-projects').style.display = 'none';
+  document.getElementById('freelancing-projects').style.display = 'none';
+  document.getElementById('contact').style.display = 'block';
+}
